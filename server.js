ee9ba635fbc36f9230e22d850121f03bedba1acb
gercorr <gercorr@gmail.com>
2016-03-27 20:18:59 +0100

    changes to config

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express    =    require('express');
+var config 	   =    require('./config');
 var app        =    express();
+
+var serverPort = config.server.port;
 
 require('./router/main')(app);
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-var server     =    app.listen(8081,function(){
-    console.log("We have started our server on port 8081");
+var server     =    app.listen(serverPort,function(){
+    console.log("We have started our server on port " + serverPort);
 });
