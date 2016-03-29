f488159c048757b55535b7c14a72d735e99b50f1
gercorr <gercorr@gmail.com>
2016-03-30 00:05:16 +0100

    added call from kanban html to node

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ require('./router/main')(app);
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('styles'));
+app.use(express.static('scripts'));
 var server     =    app.listen(serverPort,function(){
     console.log("We have started our server on port " + serverPort);
 });
