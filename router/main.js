978d464c2445547637a26ee5a968cce75076c54e
gercorr <gercorr@gmail.com>
2016-04-07 19:49:13 +0100

    cleanup

--- router/main.js
+++ router/main.js
@@ -6,51 +6,16 @@ module.exports = function(app)
 	app.get('/',function(req,res){
 		res.render('kanban.html')
 	});
-	 
-    app.get('/about',function(req,res){
-		res.render('about.html');
-    });
 	
-	app.get('/kanban', function (req, res) {
-		res.render( 'kanban.html' );
-	});
-	
-	app.get('/add_to_db', function (req, res) {
-		res.render( 'add_to_db.html' );
-	});
-	
-	app.get('/example_json', function (req, res) {
-		res.send({ "records":[ {"Name":"Alfreds Futterkiste","City":"Berlin","Country":"Germany"}, {"Name":"Ana Trujillo Emparedados y helados","City":"México D.F.","Country":"Mexico"}, {"Name":"Antonio Moreno Taquería","City":"México D.F.","Country":"Mexico"}, {"Name":"Around the Horn","City":"London","Country":"UK"}, {"Name":"B's Beverages","City":"London","Country":"UK"}, {"Name":"Berglunds snabbköp","City":"Luleå","Country":"Sweden"}, {"Name":"Blauer See Delikatessen","City":"Mannheim","Country":"Germany"}, {"Name":"Blondel père et fils","City":"Strasbourg","Country":"France"}, {"Name":"Bólido Comidas preparadas","City":"Madrid","Country":"Spain"}, {"Name":"Bon app'","City":"Marseille","Country":"France"}, {"Name":"Bottom-Dollar Marketse","City":"Tsawassen","Country":"Canada"}, {"Name":"Cactus Comidas para llevar","City":"Buenos Aires","Country":"Argentina"}, {"Name":"Centro comercial Moctezuma","City":"México D.F.","Country":"Mexico"}, {"Name":"Chop-suey Chinese","City":"Bern","Country":"Switzerland"}, {"Name":"Comércio Mineiro","City":"São Paulo","Country":"Brazil"} ] });
-	})
-	
-	app.get('/get_users', function (req, res) {
-		GetFromMongo('users2', res);
-	})	
-
 	app.get('/get_kanban', function (req, res) {
 		GetFromMongo('kanban', res);
 	})	
-    
-	app.get('/update_users', function (req, res) {
-        
-        var record = { "name": "modulus admin", "age": 42, "roles": "help" } ;
-        var newValue = { $set: { "roles.$" : "help2" } };
-        
-		UpdateMongo('users', record, newValue);
-	})	
-    
+	
 	app.get('/update_kanban', function (req, res) {
-        
         var record = req.query ;
         delete record._id;
 		ReplaceMongo('kanban', record, "ger");
 	})	
-
-	app.get('/insert_users', function (req, res) {
-		var user1 = {first_name: req.query.first_name, sur_name:req.query.last_name};
-		AddToMongo('users2', user1)
-	    res.end(JSON.stringify(user1));
-	})
 
 	app.get('/insert_kanban', function (req, res) {
 		var kanban = {state: req.query.state, name:req.query.name};		
@@ -135,9 +100,6 @@ module.exports = function(app)
 
 	}
     
-    
-    
-    
     function UpdateMongo(collectionName,  record, newValue)
 	{
 
@@ -177,7 +139,6 @@ module.exports = function(app)
 
 	}
     
-    
     function ReplaceMongo(collectionName, record, owner)
 	{
 
@@ -200,15 +161,6 @@ module.exports = function(app)
 			// Get the  collection
 			var collection = db.collection(collectionName);
             var ObjectId = mongodb.ObjectId;
-			// Insert some users
-            //try {
-            //    collection.replaceOne(
-            //        { "owner": owner},
-            //        record
-            //    );
-            //} catch (e) {
-            //    print(e);
-            //}
             
             collection.replaceOne(
                 { "owner": owner},   record              
@@ -221,16 +173,11 @@ module.exports = function(app)
 			  //Close connection
 			  db.close();
 			});
-            
-            
-            
-            
+
 		  }
+
 		});
 
 	}
-
-
-
 
 }
